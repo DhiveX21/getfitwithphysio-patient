d2ce0800e66411b46bf8f54211975b57a6ad86c3
Muhammad Ardhiansyah <muhammad.ardhiansyah@salt.ac.id>
2022-10-28 15:04:41 +0700

    fixing meta
    Former-commit-id: 1c5ca2c72ac8cf01f566cabfda9d799ff2e95c83

--- pages/_app.js
+++ pages/_app.js
@@ -16,10 +16,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           GET Fisio | One Stop Solution Women’s Health Physiotherapy Services
         </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
         <link rel="shortcut icon" href="/logo.png" />
       </Head>
       <SessionProvider session={session}>
