98953514e4c85486193ee152cc6fed913db6878f
Muhammad Ardhiansyah <ardhi.ansyah2662@gmail.com>
2022-11-13 12:07:14 +0700

    add upload Sign in telephysio Create Appointment
    Former-commit-id: 32c075efe9e202ea2ffea3ceb776955d58a475cc

--- pages/services/telephysio/index.jsx
+++ pages/services/telephysio/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Layout from "../../../components/Layout";
 import { MenuTitle } from "../../../components/Title";
 import { CardFullImage } from "../../../components/Card";
@@ -6,6 +6,10 @@ import { ButtonWithIcon } from "../../../components/Button";
 import { useRouter } from "next/router";
 
 export default function Telephysio() {
+  const [createButton, setCreateButton] = useState(false);
+  const [uploadSign, setUploadSign] = useState();
+  const signUpload = useRef();
+  console.log(uploadSign);
   const router = useRouter();
   return (
     <Layout>
@@ -40,11 +44,50 @@ export default function Telephysio() {
               with stroke patients as the study subjects.
             </p>
           </div>
+
+          <div className="note_sign w-full flex mt-[20px] flex-col">
+            <span className="text-[20px] text-center text-primary">
+              Upload Tanda Tangan Digitalmu.
+            </span>
+            <input
+              className="w-full"
+              id="sign-button"
+              type="file"
+              hidden
+              ref={signUpload}
+              accept="image/png, image/jpeg"
+              onChange={(e) =>
+                e.target.value
+                  ? (setCreateButton(true), setUploadSign(e.target.files[0]))
+                  : setCreateButton(false)
+              }
+            />
+            <label
+              className={` hover:bg-primary hover:text-white duration-200 rounded-md text-[24px] text-center px-[20px] py-[5px] cursor-pointer text-primary w-full border-2 border-dashed flex flex-col justify-center items-center leading-[1em] ${
+                uploadSign ? "border-success text-success" : "border-primary"
+              }`}
+              htmlFor="sign-button"
+            >
+              <span>
+                <img
+                  className="max-w-[50px] "
+                  src={`/images/icon/${
+                    uploadSign ? "upload-success.png" : "upload.png"
+                  }`}
+                  alt="upload"
+                />
+              </span>
+              {uploadSign ? uploadSign.name : "Upload Tanda Tangan"}
+            </label>
+          </div>
           <div className="note_button mt-[20px]">
             <ButtonWithIcon
               text="Buat Appointment"
-              classNameInject="text-[24px] leading-[18px] h-full bg-primary"
+              classNameInject={` text-white text-[24px] leading-[18px] h-full ${
+                createButton ? "bg-primary" : "bg-secondary bg-opacity-50"
+              }`}
               icon="/images/icon/appointment_icon.svg"
+              disabled={!createButton}
               click={() =>
                 router.push("/services/telephysio/createAppointment")
               }
